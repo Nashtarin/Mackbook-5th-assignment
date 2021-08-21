42ffa0efedf881fb6f4e4c7685a6a4c4fea4c77f
Nashtarin Nur <nashtarin.nur@gmail.com>
2021-08-21 17:12:22 +0600

    Done

--- JS/macbook.js
+++ JS/macbook.js
@@ -10,44 +10,33 @@ function chargedValue(termForCharge) {
     }
     else if (termForCharge == 'storage') {
         chargedId.innerText = 100;
-
     }
     else if (termForCharge == 'delivery') {
         chargedId.innerText = 20;
     }
     totalCost()
-
 }
 function totalCost() {
     const totalId = document.getElementById('totalCost');
     const totalPromo = document.getElementById('totalCostWithPromoCode');
-
-
     let total = 1299 + parseInt(document.getElementById('memory-extra-cost').innerText) + parseInt(document.getElementById('storage-extra-cost').innerText) + parseInt(document.getElementById('delivery-extra-cost').innerText);
     totalId.innerText = total;
     totalPromo.innerText = total;
-
 }
-
-
-
 document.getElementById('memory-default-button').addEventListener('click', function () {
     defaultValue('memory');
 })
 document.getElementById('storage-default-button').addEventListener('click', function () {
     defaultValue('storage');
-
 })
 document.getElementById('delivery-default-button').addEventListener('click', function () {
     defaultValue('delivery');
 })
 document.getElementById('memory-charge-button').addEventListener('click', function () {
     chargedValue('memory');
-
 })
 document.getElementById('storage-charge-button').addEventListener('click', function () {
     chargedValue('storage');
-
 })
 document.getElementById('delivery-charge-button').addEventListener('click', function () {
     chargedValue('delivery');
@@ -57,10 +46,16 @@ document.getElementById('storage-another-charge-button').addEventListener('click
     anotherChargedId.innerText = 180;
     totalCost();
 })
-// let total = parseInt(document.getElementById('totalCost').innerText);
+document.getElementById("promoButton").addEventListener('click', function () {
+    const promoCodeID = document.getElementById('promoInput');
+    const promocode = promoCodeID.value;
+    let totalPromo = parseFloat(document.getElementById('totalCostWithPromoCode').innerText);
+    if (promocode.toLowerCase() == 'stevekaku') {
+        totalPromo = totalPromo - totalPromo * .20;
+        document.getElementById('totalCostWithPromoCode').innerText = totalPromo;
+        document.getElementById('promoInput').value = '';
+        document.getElementById("promoButton").disabled = true;
 
-// total = total + parseInt(document.getElementById('memory-extra-cost').innerText) + parseInt(document.getElementById('storage-extra-cost').innerText) + parseInt(document.getElementById('delivery-extra-cost').innerText);
-// console.log(total);
-// //document.getElementById('totalCost').innerText = total;
-// let totalPromo = parseFloat(document.getElementById('totalCostWithPromoCode').innerText);
+    }
+})
 
